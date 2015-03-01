967585659f9686bdf574bf884dacfad078be39ec
Dan <armgod@gmail.com>
2015-03-01 19:41:31 +0800

    创建

---
+++ loading_js.js
@@ -0,0 +1,5 @@
+javascript: (function () { 
+var jsCode = document.createElement('script'); 
+jsCode.setAttribute('src', 'http://hbdesign-demo.b0.upaiyun.com/playHbGif-v0.4/playHbGif.js'); 
+document.body.appendChild(jsCode); 
+}()); 
---
+++ playHbGif.js
@@ -0,0 +1,18 @@
+﻿// ==UserScript==
+// @name         Play Them all!
+// @namespace    http://armgod.com/
+// @version      0.4
+// @description  Play all the Gif images at huaban.com.
+// @author       Dan
+// @include      *://*huaban.com/*
+// @copyright    2015
+// @grant        none
+// ==/UserScript==
+
+var gGif = function() {
+    $$('.pin a.img img').each(function(img){img.src = img.src.replace(/fw236$/, 'fw236g'); img.src = img.src.replace(/fw320$/, 'fw320g')});
+    $$('.pin .img .gif-icon').setStyle('display','none');
+}
+window.addEvent('domready', gGif); 
+window.addEvent('scroll', gGif); 
+window.addEvent('click', gGif); 
